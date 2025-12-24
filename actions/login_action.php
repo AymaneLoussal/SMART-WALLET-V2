d1a8cd8d9517b894a158b06858df323aa5c756f5
Aymane Loussal <aymanloussal552@gmail.com>
2025-12-24 16:39:37 +0100

    feat fetch the login data and check it for log in

---
+++ actions/login_action.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require_once "../models/User.php";
+
+$user = new User();
+
+try {
+
+    $user->login($_POST["email"], $_POST["password"]);
+
+    $_SESSION["user_id"] = $user->getUserId();
+    $_SESSION["user_email"] = $_POST["email"];
+
+    header("Location: ../dashboard.php");
+    exit;
+
+} catch (Exception $e) {
+
+    header("Location: ../login.php?error=" . urlencode($e->getMessage()));
+    exit;
+}
